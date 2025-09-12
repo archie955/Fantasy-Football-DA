18a5b86c5b71f6b214eb110fc232c3e5dd198c99
archie955 <archiesmedley@gmail.com>
2025-09-12 16:02:33 +0100

    Add database model reformat files extend config file

--- src/api/config.py
+++ src/api/config.py
@@ -2,6 +2,11 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     API_KEY: str
+    database_hostname: str
+    database_port: str
+    database_password: str
+    database_name: str
+    database_username: str
 
     class Config:
         env_file = ".env"
